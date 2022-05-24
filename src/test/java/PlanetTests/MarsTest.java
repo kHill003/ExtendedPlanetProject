e7af501347f8b8ba0eaf938f492fa756139be38c
kHill003 <kris.hillquist@codeclan.com>
2022-05-24 14:36:45 +0100

    added tests

--- src/test/java/PlanetTests/MarsTest.java
+++ src/test/java/PlanetTests/MarsTest.java
@@ -4,6 +4,7 @@ import Planets.Mars;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MarsTest {
@@ -21,5 +22,23 @@ public class MarsTest {
 //    @Test I wrote a test that did fail.  I did it on purpose to prove that the method works
 //    public void hasNameWillFail(){
 //        assertEquals("Jupiter", mars.getName());
+//    }
+//Again, a fail test to prove it works
+//@Test
+//public void hasSizeFail(){
+//    assertEquals(80, mars.getPlanetSize());}
+//}
+@Test
+public void hasSize(){
+    assertEquals(10000, mars.getPlanetSize());
+}
+
+    @Test
+    public void isInhabited(){
+        assertEquals(true, mars.isInhabited());
+    }
+//    @Test  To tsst it will fail if incorrect. Yes, it fails
+//    public void isInhabiteFaild(){
+//        assertEquals(false, mars.isInhabited());
 //    }
 }
